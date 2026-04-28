e1f73ceae2fa13b38dad9ea3b0a159bbd3735803
Rahul Prasad <83229530+Rahul543-ux@users.noreply.github.com>
2026-04-28 15:26:59 +0530

    main.py

--- backend/main.py
+++ backend/main.py
@@ -432,4 +432,7 @@ def free_table(table_id: str):
     if tid not in tables_db:
         raise HTTPException(404, f"Table '{tid}' nahi mila!")
     tables_db[tid].update({"occupied":False,"guests":0,"status":"free"})
+    for o in orders_db.values():
+    if o["table_id"] == tid and o["status"] == "delivered":
+        o["status"] = "completed"
     return {"status":"ok","message":f"✅ Table {tid} free hai!","table":tables_db[tid]}
